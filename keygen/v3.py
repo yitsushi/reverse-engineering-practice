75bb7ac85a9b9a54690fcf23448196836ce73b02
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2018-03-28 15:22:24 +0200

    Add details on keygen [how to build it with asm references]

--- keygen/v3.py
+++ keygen/v3.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python3
 
+# ; token length = 27
+# 0x100000e5a      488b75f0       mov rsi, qword [local_10h]
+# 0x100000e5e      488b7e08       mov rdi, qword [rsi + 8]
+# 0x100000e62      8945dc         mov dword [local_24h], eax
+# 0x100000e65      e8d8000000     call sym.imp.strlen        ; size_t strlen(const char *s)
+# 0x100000e6a      4883f81b       cmp rax, 0x1b              ; 27
+#
+# ; 4 blocks with a separator between them
+# 0x100000e9f      837de804       cmp dword [local_18h], 4
+#
+# ; one block contains 6 characters
+# 0x100000eb0      837de406       cmp dword [local_1ch], 6
+#
+# ; sum of all the characters 0x709 (1081)
+# 0x100000ef7      817dec090700.  cmp dword [local_14h], 0x709 ; [0x709:4]=-1 ; 1801
+#
+# XXXXXX XXXXXX XXXXXX XXXXXX
+#
+# ^^ easier to pass as an argument with non-space separator, let's choose '-'
+# but can be anything else
+#
+# XXXXXX-XXXXXX-XXXXXX-XXXXXX
+#
+# Avg. block sum is 450.25
+#
+# 4 * 450 = 1080
+#
+# 450 / 6 = 75 ; 0x4b => 'K'
+#
+# we have to increase one of them: 'L'
+#
+# KKKKKK-KKKKKK-KKKKKK-KKKKKL
+# 
+# still valid keys (examples on separator):
+# KKKKKKaKKKKKKaKKKKKKaKKKKKL
+# KKKKKK/KKKKKK/KKKKKK/KKKKKL
+
+
 import string
 import random
 
